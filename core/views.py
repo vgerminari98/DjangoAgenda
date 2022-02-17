7354cebaaeb1a2d4e415191e6e38f5e34c7e7290
vgerminari98 <vgerminari98@outlook.com>
2022-02-17 18:52:27 -0300

    autenticação de login

--- core/views.py
+++ core/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 # def index(request):
 #     return redirect('/agenda')
 
+def login_user(request):
+    return render(request,'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+    return redirect('/')
+
+@login_required(login_url='login/')
 def listaEventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
-    return render(request, 'agenda.html', dados)
+    return render(request,'agenda.html', dados)
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
